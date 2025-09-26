9b6e5c1670c793147f08546ee91f9344306fdb38
SAMKOREA <123339750+Jeonmingang@users.noreply.github.com>
2025-09-26 13:06:53 +0900

    Delete src/main directory

--- src/main/java/com/minkang/uto/integrations/WorldBorderIntegration.java
+++
@@ -1,8 +0,0 @@
-﻿package com.minkang.uto.integrations;
-import org.bukkit.Bukkit;
-import org.bukkit.World;
-import org.bukkit.WorldBorder;
-public class WorldBorderIntegration {
-    public static boolean hasPlugin() { return Bukkit.getPluginManager().getPlugin(\"WorldBorder\") != null; }
-    public static double getRadius(World world) { WorldBorder wb = world.getWorldBorder(); return wb.getSize() / 2.0; }
-}
